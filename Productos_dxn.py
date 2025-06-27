1f38e7635fa5845a0c20375a72f1ed837a31c1b6
Sayra199 <sayrascareth95@gmail.com>
2025-06-27 07:27:06 -0600

    Create Productos_dxn.py
    Insertar productos y valorarlos en dxn
    
    Signed-off-by: Sayra199 <sayrascareth95@gmail.com>

---
+++ Productos_dxn.py
@@ -0,0 +1,7 @@
+INSER INTO PRODUCTS (PRODUCTS_Id INT PRIMARY KEY,
+category) VALUES
+(1.¨GANODERMA¨,hongos medicinalescon multiple beneficios:20,00,30,"suplementos")
+(2."cordiseps", energetizante natural:25,00,15, "suplementos")
+(3."cafe lingzhi",cafe extracto de ganoderma:,12,50,80,"bebidas")
+(4."spirulina", alga  de alto contenido de proteina:18,00, "suplementos")
+print("productos_id_dxn¨)
